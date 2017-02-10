d318f80d98fb96026dac5835d0562909efd4d415
Florian Csizmazia <florian.csizmazia@gugler.at>
2017-02-10 09:16:49 +0100

    added production stuff to webpack config and created bundle.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
-
+path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+  devtool: 'cheap-module-source-map',
   entry: './index.js',
 
   output: {
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: ''
   },
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      }
     ]
-  }
+  },
+  plugins: [
+   new webpack.DefinePlugin({
+     'process.env': {
+       'NODE_ENV': JSON.stringify('production')
+     }
+   })
+ ]
 }
